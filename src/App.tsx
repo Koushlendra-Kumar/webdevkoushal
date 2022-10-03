67bbd0d7c1c1961afe60442f3c6e80a1c9e23af8
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-10-03 06:00:46 +0530

    Create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <Header />
+      <h1 className='text-3xl font-bold'>Welcome to my Portfolio!</h1>
     </div>
   );
 }
