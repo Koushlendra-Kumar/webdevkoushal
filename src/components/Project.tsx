e2e2a6c6014407225c3b27a3e4e63861bacb75e9
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-10-12 05:59:42 +0530

    Add live preview and source code button

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,31 +1,24 @@
 import React from 'react';
-import logo from '../images/logo192.png';
 
 function Project() {
   return (
-    <div className="max-w-sm w-full lg:max-w-full lg:flex">
-        <div className="project-screenshot h-48 lg:h-auto lg:w-48 flex-none bg-cover lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
-        </div>
-        <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+    <div className="max-w-xl mx-auto m-10 pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16 lg:flex">
+        <div className="project-screenshot h-48 lg:h-auto lg:w-48 flex-none bg-cover lg:rounded-t-none lg:rounded-l text-center overflow-hidden"></div>
+        <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
             <div className="mb-8">
-            <p className="text-sm text-gray-600 flex items-center">
-                <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                </svg>
-                Members only
-            </p>
-            <div className="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-            <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+                <div className="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
+                <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
             </div>
-            <div className="flex items-center">
-            <img className="w-10 h-10 rounded-full mr-4" src={logo} alt="Avatar" />
-            <div className="text-sm">
-                <p className="text-gray-900 leading-none">Jonathan Reinink</p>
-                <p className="text-gray-600">Aug 18</p>
-            </div>
+            <div className="flex items-center justify-around">
+                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    Preview
+                </button>
+                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    Source Code
+                </button>
             </div>
         </div>
-        </div>
+    </div>
   );
 }
 
