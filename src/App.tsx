449ea23c74769d1c8a21ce7559b14a06b364f705
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-11-25 16:33:26 +0530

    Add source code and preview links to projects

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,24 @@ function App() {
     <div className="App">
       <Header />
       <Intro />
-      <Project />
-      <Project />
-      <Project />
+      <Project 
+          title='My Portfolio' 
+          description='My MERN stack portfolio.' 
+          sourceLink='https://github.com/Koushlendra-Kumar/portfolio-site' 
+          previewLink='https://koushlendra-kumar.github.io/portfolio-site/'
+          />
+      <Project 
+          title='Members Only' 
+          description='User authentication web app.' 
+          sourceLink='https://github.com/Koushlendra-Kumar/Members-only' 
+          previewLink='https://topclub.herokuapp.com/'
+          />
+      <Project 
+          title='Modern Furniture' 
+          description='Api to perform CRUD operations.' 
+          sourceLink='https://github.com/Koushlendra-Kumar/Modern-furniture' 
+          previewLink='https://modern-furniture-production.up.railway.app/'
+          />
       <Contact />
       <Footer />
     </div>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import logo from '../images/logo192.png';
+import logo from '../images/projectUI.png';
 
-function Project() {
+function Project(props: any) {
   return (
     <div className="flex justify-center mt-6">
         <div className="rounded-lg shadow-lg bg-white max-w-sm">
-            <a href="#!">
-            <img className="rounded-t-lg" src={logo} alt=""/>
+            <a href="#!" className='flex justify-center items-center'>
+            <img className="h-52 w-52 rounded-t-lg" src={logo} alt=""/>
             </a>
             <div className="p-6">
-                <h5 className="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                <h5 className="text-gray-900 text-xl font-medium mb-2">{props.title}</h5>
                 <p className="text-gray-700 text-base mb-4">
-                    Some quick example text to build on the card title and make up the bulk of the card's
-                    content.
+                    {props.description}
                 </p>
-                <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                <a href={props.sourceLink} rel='noreferrer' target='_blank'>
+                <button type="button" className="mr-5 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Source Code</button>
+                </a>
+                <a href={props.previewLink} rel='noreferrer' target='_blank'>
+                <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Preview</button>
+                </a>
             </div>
         </div>
     </div>
