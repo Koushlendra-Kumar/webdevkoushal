6883a5c1ebff0a02eb43ca4e5b1feacb980f3132
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-11-26 10:06:48 +0530

    Add fontawesome github icon

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -14,10 +14,13 @@ function Project(props: any) {
                     {props.description}
                 </p>
                 <a href={props.sourceLink} rel='noreferrer' target='_blank'>
-                <button type="button" className="mr-5 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Source Code</button>
+                  <button type="button" className="mr-5 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                  <i className="fa-brands fa-github fa-xl mr-2"></i>
+                    Source Code
+                  </button>
                 </a>
                 <a href={props.previewLink} rel='noreferrer' target='_blank'>
-                <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Preview</button>
+                <button type="button" className=" inline-block px-6 py-2.5 bg-yellow-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Preview</button>
                 </a>
             </div>
         </div>
