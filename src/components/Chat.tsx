9f66ca52a7ad6a8c020fe9daa0a9949cd9698c76
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-12-20 12:52:28 +0530

    Create component for live chat

---
+++ src/components/Chat.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Popup from 'reactjs-popup';
+
+function Chat() {
+    return (
+        <Popup trigger={
+            <button className='fixed bottom-0 right-0 m-8 p-4 rounded-full text-white flex justify-center items-center bg-lime-500'>
+                <i className="fa-solid fa-2x fa-message"></i>
+            </button>
+            } position='top center'>
+            <div className=''>
+               Hello
+            </div>
+        </Popup>
+    )
+}
+
+export default Chat;
