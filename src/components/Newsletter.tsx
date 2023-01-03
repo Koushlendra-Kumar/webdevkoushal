365433b2467c1240f76ef0181ba80f113469a5e5
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-01-03 15:33:42 +0530

    :lipstick: Update ui for newsletter form

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 function Newsletter() {
     return (
-        <div className=' fixed top-0 left-0 right-0 m-auto max-w-sm border-2 border-solid border-black font-sans p-5 bg-black'>
-            <div className='w-full flex justify-center items-center'>
-                <h1 className='text-xl font-bold text-green-400'>Coming Soon</h1>
+        <div className='fixed top-0 left-0 right-0 m-auto max-w-sm border border-solid border-black font-sans p-5 rounded'>
+            <div className='w-full flex justify-center items-center flex-col'>
+                <h1 className='font-bold font-serif text-2xl'>Newsletter</h1>
+                <form className='flex flex-col justify-start items-center gap-5 mt-5'>
+                    <input type='email' placeholder='Enter your email' className='box-border p-2 border h-10 w-full'/>
+                    <button type="submit" className='inline-block text-sm px-4 py-2 leading-none border rounded font-medium bg-red-500 text-white hover:text-white hover:bg-red-600 hover:underline mt-4 lg:mt-0'>Subscribe</button>
+                </form>
             </div>
         </div>
     )
