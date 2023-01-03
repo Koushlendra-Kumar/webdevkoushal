dca236834fef987710487d884318fe4e2a7332b4
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-01-03 15:49:16 +0530

    :lipstick: Change border color for input field

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -6,7 +6,7 @@ function Newsletter() {
             <div className='w-full flex justify-center items-center flex-col'>
                 <h1 className='font-bold font-serif text-2xl'>Newsletter</h1>
                 <form className='flex flex-col justify-start items-center gap-5 mt-5'>
-                    <input type='email' placeholder='Enter your email' className='box-border p-2 border h-10 w-full'/>
+                    <input type='email' placeholder='Enter your email' className='box-border p-2 border h-10 w-full border-gray-400'/>
                     <button type="submit" className='inline-block text-sm px-4 py-2 leading-none border rounded font-medium bg-red-500 text-white hover:text-white hover:bg-red-600 hover:underline mt-4 lg:mt-0'>Subscribe</button>
                 </form>
             </div>
